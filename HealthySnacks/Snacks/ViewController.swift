290486e0bee8e95e659354873c1127dcf6f3c144
Pritesh Desai <iampriteshdesai@gmail.com>
2018-11-12 23:48:49 -0800

    Healthy or Unhealthy App v1.0
    My first machine learning app.

---
+++ HealthySnacks/Snacks/ViewController.swift
@@ -0,0 +1,150 @@
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+  
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var cameraButton: UIButton!
+  @IBOutlet var photoLibraryButton: UIButton!
+  @IBOutlet var resultsView: UIView!
+  @IBOutlet var resultsLabel: UILabel!
+  @IBOutlet var resultsConstraint: NSLayoutConstraint!
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            // 1
+            let healthySnacks = HealthySnacks()
+            // 2
+            let visionModel = try VNCoreMLModel(for: healthySnacks.model)
+            // 3
+            let request = VNCoreMLRequest(model: visionModel, completionHandler: {
+                [weak self] request, error in
+                self?.processObservations(for: request, error: error)
+            })
+            // 4
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        }
+        catch {
+            fatalError("Failed to create model: \(error)")
+        }
+    }()
+
+  var firstTime = true
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    resultsView.alpha = 0
+    resultsLabel.text = "choose or take a photo"
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    // Show the "choose or take a photo" hint when the app is opened.
+    if firstTime {
+      showResultsView(delay: 0.5)
+      firstTime = false
+    }
+  }
+  
+  @IBAction func takePicture() {
+    presentPhotoPicker(sourceType: .camera)
+  }
+
+  @IBAction func choosePhoto() {
+    presentPhotoPicker(sourceType: .photoLibrary)
+  }
+
+  func presentPhotoPicker(sourceType: UIImagePickerControllerSourceType) {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.sourceType = sourceType
+    present(picker, animated: true)
+    hideResultsView()
+  }
+
+  func showResultsView(delay: TimeInterval = 0.1) {
+    resultsConstraint.constant = 100
+    view.layoutIfNeeded()
+
+    UIView.animate(withDuration: 0.5,
+                   delay: delay,
+                   usingSpringWithDamping: 0.6,
+                   initialSpringVelocity: 0.6,
+                   options: .beginFromCurrentState,
+                   animations: {
+      self.resultsView.alpha = 1
+      self.resultsConstraint.constant = -10
+      self.view.layoutIfNeeded()
+    },
+    completion: nil)
+  }
+
+  func hideResultsView() {
+    UIView.animate(withDuration: 0.3) {
+      self.resultsView.alpha = 0
+    }
+  }
+
+  func classify(image: UIImage) {
+    guard let ciImage = CIImage(image: image)
+        else {
+            print("Unable to create image")
+            return
+        }
+    
+    let orientation = CGImagePropertyOrientation(image.imageOrientation)
+    
+    DispatchQueue.global(qos: .userInitiated).async {
+        let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+        
+        do {
+            try handler.perform([self.classificationRequest])
+        }
+        catch {
+            print("Failed to perform classification: \(error)")
+        }
+    }
+  }
+    
+    func processObservations(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            if let results = request.results as? [VNClassificationObservation] {
+                if results.isEmpty {
+                    self.resultsLabel.text = "Nothing found"
+                }
+                else if results[0].confidence < 0.8 {
+                    self.resultsLabel.text = "Not sure"
+                }
+                else {
+                    self.resultsLabel.text =    String(
+                                format: "%@ %.1f%%",
+                                results[0].identifier,
+                                results[0].confidence * 100)
+                }
+            }
+            else if let error = error {
+                self.resultsLabel.text = error.localizedDescription
+            }
+            else {
+                self.resultsLabel.text = "??"
+            }
+            
+            self.showResultsView()
+        }
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    picker.dismiss(animated: true)
+
+    let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+    imageView.image = image
+
+    classify(image: image)
+  }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Healthy or Unhealthy Snack
+
+This is my first Machine Learning app. I'm following the tutorial from 'Machine Learning by Tutorials by Ray Wenderlich'.
+
+It is a simple app, you point the phone at your plate and it tells you if it is healthy or unhealthy.
+
+The ML model which makes the decision has been downloaded from the Apple website. The beauty of ML is that you can replace the ML model with some other kind. Eg. a model which identifies the kind of car.
+The rest of the app can stay the same and simply by changing the model you can get an app which identifies other things.
